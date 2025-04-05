7583b4a9f0e63e59d2089e11584472210d4ea6fe
Sahibzada Issar Ali <issaralisia@gmail.com>
2025-04-05 09:40:23 +0500

    button fixed

--- resources/views/promp-form.blade.php
+++ resources/views/promp-form.blade.php
@@ -6,8 +6,8 @@
     <title>Welcome</title>
     @vite('resources/css/app.css')
 </head>
-<body>
-   <div class="w-7xl mx-auto h-screen flex px-50  flex-col bg-gray-100 py-3" style="justify-content: center; align-items: center;">
+<body class="p-3">
+   <div class="w-7xl mx-auto h-screen flex px-50  flex-col bg-gray-100 py-3 rounded-sm border-1 border-blue-300" style="justify-content: center; align-items: center;">
     <div class="mb-4"><h1 class="text-2xl pt-4">Open AI Form</h1></div>
     <form class="block w-full" method="POST" action="{{ route('chat') }}" enctype="multipart/form-data">
         @csrf
@@ -15,17 +15,19 @@
         {{-- prompt  --}}
         <div class="mb-4 w-full">
             <label for="name" class="block text-gray-700 text-sm font-bold mb-2">Write Serivce Prompt</label>
-            <textarea id="prompt" name="prompt" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required></textarea>
+            <textarea id="prompt" name="prompt" class="shadow appearance-none rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-1 border-blue-200" required></textarea>
         </div>
 
         {{-- logo  --}}
         <div class="mb-4 w-full">
             <label for="logo" class="block text-gray-700 text-sm font-bold mb-2">Logo</label>
-            <input id="logo" type="file" name="logo" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" required />
+            <input id="logo" type="file" name="logo" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-blue-200" required />
         </div>
 
         {{-- image  --}}
-        <input type="submit" value="Submit" class="bg-blue-500 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" />
+        <div class="w-full">
+        <input type="submit" value="Submit" class="bg-blue-500 hover:bg-blue-500 text-white  py-2 px-4 rounded focus:outline-none focus:shadow-outline block w-full" />
+        </div>
         </form>    
    </div>
 </body>
